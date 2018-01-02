b22f25c393e341d4757ec35ff76089539caf229a
Amitav Roy <reachme@amitavroy.com>
2018-01-02 09:28:57 +0530

    CSS doubts were fixed by help of @dileepmeruvala

--- app/Models/Presenters/UserPresenter.php
+++ app/Models/Presenters/UserPresenter.php
@@ -7,7 +7,7 @@ trait UserPresenter
     public function profilePic(): string
     {
         $string = ucfirst(substr($this->name, 0, 1));
-        $markup = "<div class='profile-pic'>{$string}</div>";
-        return '';
+        $markup = "<div class='profile-pic'><span>{$string}</span></div>";
+        return $markup;
     }
 }
