55baef67465a1187d8b3f9ba4b7c467b2192acf9
Monish <monish.narwani@focalworks.in>
2018-01-04 00:07:08 +0530

    comments table created, relations defined, factory methods defined, seeding done

---
+++ database/seeds/CommentsSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CommentsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        for ($i = 0; $i < 10; $i++) {
+            factory(\App\Models\Comment::class)->create();
+        }
+    }
+}
