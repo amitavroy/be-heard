65c7b4cc2680c8741765b074c8c95482eb6f7b1c
Amitav Roy <reachme@amitavroy.com>
2018-01-06 21:43:17 +0530

    comments are now part of the conversations instead of independent comments and hence seeder is not required

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,9 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call(UsersTableSeeder::class);
-         $this->call(InvitesTableSeeder::class);
-         $this->call(CategoriesTableSeeder::class);
-         $this->call(CommentsSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(InvitesTableSeeder::class);
+        $this->call(CategoriesTableSeeder::class);
+        // $this->call(CommentsSeeder::class);
     }
 }
