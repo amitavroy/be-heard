619ecac433e23a369f342c2cf97eb0d3ac006f8a
Monish <monish.narwani@focalworks.in>
2018-01-14 13:54:01 +0530

    done with profile listing of conversations & comments

--- resources/views/minimal/partials/comments-teaser.blade.php
+++ resources/views/minimal/partials/comments-teaser.blade.php
@@ -1,9 +1,8 @@
-<div class="comments teaser">
+<div class="comment teaser">
     <div class="clearfix">
         <span class="author pull-left">{!! $comment->user->profilePic() !!}</span>
         <h3>
-            {{--{{route('conversation.view', $conversation->slug)}}--}}
-            <a href="">{{$comment->body}}</a>
+            <a href="{{route('conversation.view', $comment->commentable->slug)}}">{{$comment->body}}</a>
         </h3>
     </div>
 </div>
